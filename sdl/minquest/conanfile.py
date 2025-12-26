65b81f8cb047ac695d74bba6f10db69b2be4cb90
Terence Tan <terence@bigbadrobots.com>
2025-12-26 08:20:54 +0800

    Re-organize workspace

--- sdl/minquest/conanfile.py
+++ sdl/minquest/conanfile.py
@@ -1,9 +1,13 @@
 from conan import ConanFile
+from conan.tools.cmake import cmake_layout
 
 class MinquestRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeToolchain", "CMakeDeps"
 
     def requirements(self):
-         self.requires("sdl/3.2.20")
-         self.requires("imgui/1.92.5")
+        self.requires("sdl/3.2.20")
+        self.requires("imgui/1.92.5")
+
+    def layout(self):
+        cmake_layout(self)
