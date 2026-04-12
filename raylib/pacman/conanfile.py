715242f716ae635a311312bf07d9f71ae4ed8f12
Terence Tan <terence@bigbadrobots.com>
2026-04-12 18:40:54 +0800

    Initial push, raylib

---
+++ raylib/pacman/conanfile.py
@@ -0,0 +1,14 @@
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout
+
+class MinquestRecipe(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps"
+
+    def requirements(self):
+        self.requires("raylib/5.5")
+        self.requires("spdlog/1.16.0")
+        self.requires("nlohmann_json/3.12.0")
+
+    def layout(self):
+        cmake_layout(self)
