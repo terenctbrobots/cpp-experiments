33e1480b19cc892a8f4f6b56761c2819780351e5
Terence <terence@bidrobots.com>
2026-05-04 17:06:33 +0800

    Quest barebones

---
+++ sdl/quest/conanfile.py
@@ -0,0 +1,16 @@
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout
+
+class QuestRecipe(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps"
+
+    def requirements(self):
+        self.requires("sdl/3.2.20")
+        self.requires("sdl_image/3.2.4")
+        self.requires("imgui/1.92.5")
+        self.requires("spdlog/1.16.0")
+        self.requires("nlohmann_json/3.12.0")
+
+    def layout(self):
+        cmake_layout(self)
