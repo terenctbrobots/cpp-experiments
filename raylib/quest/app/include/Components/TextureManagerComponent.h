e3aa692be49f454e682e4b5245544b1e22d48085
Terence Tan <terence.tan@bigbadrobots.com>
2026-05-30 13:34:45 +0800

    Simplify TextureManager

--- raylib/quest/app/include/Components/TextureManagerComponent.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "raylib.h"
-#include <unordered_map>
-
-struct TextureManagerComponent
-{
-    std::unordered_map<unsigned int, Texture2D> m_TextureList;
-};
--- raylib/quest/app/src/TextureManager.cpp
+++ raylib/quest/app/src/TextureManager.cpp
@@ -1,39 +1,17 @@
 #include "TextureManager.h"
 
-#include "Components/TextureManagerComponent.h"
-
+static std::unordered_map<unsigned int, Texture2D> TextureList;
 
 bool TextureManager::Add(gaia::ecs::World& world, Texture2D& newTexture)
 {
-    gaia::ecs::Entity textureManagerEntity = Get(world);
-
-    if (textureManagerEntity == gaia::ecs::EntityBad)
+    if (TextureList.count(newTexture.id) == 0)
     {
-        textureManagerEntity = world.add();
-        world.add<TextureManagerComponent>(textureManagerEntity);
-    }
-
-    auto& textureManagerComponet = world.set<TextureManagerComponent>(textureManagerEntity);
-
-    if (textureManagerComponet.m_TextureList.count(newTexture.id) == 0)
-    {
-        textureManagerComponet.m_TextureList.emplace(newTexture.id, newTexture);
+        TextureList.emplace(newTexture.id, newTexture);
         return true;
     }
 
     return false;
 }
-
-gaia::ecs::Entity TextureManager::Get(gaia::ecs::World& world)
-{
-    world.query().all<TextureManagerComponent>().each([&](gaia::ecs::Entity e)
-    {
-        return e;
-    });
-
-    return gaia::ecs::EntityBad;
-}
-
 
 bool TextureManager::Remove(gaia::ecs::World& world, Texture2D& oldTexture)
 {
@@ -42,15 +20,9 @@ bool TextureManager::Remove(gaia::ecs::World& world, Texture2D& oldTexture)
 
 void TextureManager::Clear(gaia::ecs::World& world)
 {
-    gaia::ecs::Entity textureManagerEntity = Get(world);
-
-    if (textureManagerEntity != gaia::ecs::EntityBad)
+    for (const auto& [key, texture] : TextureList)
     {
-        auto& textureManagerComponent = world.get<TextureManagerComponent>(textureManagerEntity);
-
-        for (const auto& [key, texture] : textureManagerComponent.m_TextureList)
-        {
-            UnloadTexture(texture);
-        }
+        UnloadTexture(texture);
     }
+    TextureList.clear();
 }
